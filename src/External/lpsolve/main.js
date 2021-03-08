d3e10cc30911d52e12abbf004cba8ad4c1db8117
Ben Hancock <hancocb@purdue.edu>
2021-03-07 21:55:46 -0500

    comment out fs in main

--- src/External/lpsolve/main.js
+++ src/External/lpsolve/main.js
@@ -6,7 +6,6 @@
 /*global exports*/
 /*global Promise*/
 
-
 // LP SOLVE CLI REFERENCE:
 // http://lpsolve.sourceforge.net/5.5/lp_solve.htm
 //
@@ -14,111 +13,106 @@
 
 // var reformat = require("./Reformat.js");
 
-exports.reformat = require("./Reformat.js");
+exports.reformat = require('./Reformat.js');
 
-function clean_data(data){
+function clean_data(data) {
+  //
+  // Clean Up
+  // And Reformatting...
+  //
+  data = data.replace('\\r\\n', '\r\n');
 
-    //
-    // Clean Up
-    // And Reformatting...
-    //
-    data = data.replace("\\r\\n","\r\n");
+  data = data.split('\r\n');
+  data = data
+    .filter(function (x) {
+      var rx;
 
+      //
+      // Test 1
+      rx = new RegExp(' 0$', 'gi');
+      if (rx.test(x) === true) {
+        return false;
+      }
 
-    data = data.split("\r\n");
-    data = data.filter(function(x){
-        
-        var rx;
-        
-        //
-        // Test 1
-        rx = new RegExp(" 0$","gi");
-        if(rx.test(x) === true){
-            return false;
-        }
+      //
+      // Test 2
+      rx = new RegExp('\\d$', 'gi');
+      if (rx.test(x) === false) {
+        return false;
+      }
 
-        //
-        // Test 2
-        rx = new RegExp("\\d$","gi");
-        if(rx.test(x) === false){
-            return false;
-        }
-        
-
-        return true;
+      return true;
     })
-    .map(function(x){
-        return x.split(/\:{0,1} +(?=\d)/);
+    .map(function (x) {
+      return x.split(/\:{0,1} +(?=\d)/);
     })
-    .reduce(function(o,k,i){
-        o[k[0]] = k[1];
-        return o;
-    },{});
-    
-    return data;
+    .reduce(function (o, k, i) {
+      o[k[0]] = k[1];
+      return o;
+    }, {});
+
+  return data;
 }
 
-
-
-
-
-exports.solve = function(model){
+exports.solve = function (model) {
+  //
+  return new Promise(function (res, rej) {
     //
-    return new Promise(function(res, rej){
-        //
-        // Exit if we're in the browser...
-        //
-        if(typeof window !== "undefined"){
-            rej("Function Not Available in Browser");
-        }
-        //
-        // Convert JSON model to lp_solve format
-        //
-        var data = require("./Reformat.js")(model);
-        
-        
-        if(!model.external){
-            rej("Data for this function must be contained in the 'external' attribute. Not seeing anything there.");
-        }
-        
-        // 
-        // In the args, they *SHALL* have provided an executable
-        // path to the solver they're piping the data into
-        //
-        if(!model.external.binPath){
-            rej("No Executable | Binary path provided in arguments as 'binPath'");
-        }
-        
-        //
-        // They also need to provide an arg_array
-        //
-        if(!model.external.args){
-            rej("No arguments array for cli | bash provided on 'args' attribute");
-        }
-        
-        //
-        // They also need a tempName so we know where to store
-        // the temp file we're creating...
-        //
-        if(!model.external.tempName){
-            rej("No 'tempName' given. This is necessary to produce a staging file for the solver to operate on");
-        }
-        
-        
-        
-        //
-        // To my knowledge, in Windows, you cannot directly pipe text into
-        // an exe...
-        //
-        // Thus, our process looks like this...
-        //
-        // 1.) Convert a model to something an external solver can use
-        // 2.) Save the results from step 1 as a temp-text file
-        // 3.) Pump the results into an exe | whatever-linux-uses
-        // 4.) 
-        // 
-        //
-        
+    // Exit if we're in the browser...
+    //
+    if (typeof window !== 'undefined') {
+      rej('Function Not Available in Browser');
+    }
+    //
+    // Convert JSON model to lp_solve format
+    //
+    var data = require('./Reformat.js')(model);
+
+    if (!model.external) {
+      rej(
+        "Data for this function must be contained in the 'external' attribute. Not seeing anything there."
+      );
+    }
+
+    //
+    // In the args, they *SHALL* have provided an executable
+    // path to the solver they're piping the data into
+    //
+    if (!model.external.binPath) {
+      rej("No Executable | Binary path provided in arguments as 'binPath'");
+    }
+
+    //
+    // They also need to provide an arg_array
+    //
+    if (!model.external.args) {
+      rej("No arguments array for cli | bash provided on 'args' attribute");
+    }
+
+    //
+    // They also need a tempName so we know where to store
+    // the temp file we're creating...
+    //
+    if (!model.external.tempName) {
+      rej(
+        "No 'tempName' given. This is necessary to produce a staging file for the solver to operate on"
+      );
+    }
+
+    //
+    // To my knowledge, in Windows, you cannot directly pipe text into
+    // an exe...
+    //
+    // Thus, our process looks like this...
+    //
+    // 1.) Convert a model to something an external solver can use
+    // 2.) Save the results from step 1 as a temp-text file
+    // 3.) Pump the results into an exe | whatever-linux-uses
+    // 4.)
+    //
+    //
+
+    /*
         var fs = require("fs");
         
         fs.writeFile(model.external.tempName, data, function(fe, fd){
@@ -174,12 +168,9 @@ exports.solve = function(model){
                 });
             }
         });
-    });
+        */
+  });
 };
-
-
-
-
 
 /*
 model.external = {
